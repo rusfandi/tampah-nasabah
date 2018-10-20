2a141f5253bc01e7f6655d6bd0beb475e9f971df
rusfandi <fanira.fandi@gmail.com>
2018-10-20 19:00:38 +0700

    Initial commit

---
+++ precache-manifest.966136711ec4ca5f8ff26c6d9347f026.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "00738663e85bc7d72f4c",
+    "url": "/static/js/main.00738663.chunk.js"
+  },
+  {
+    "revision": "bf0697c2f096d27a2156",
+    "url": "/static/js/1.bf0697c2.chunk.js"
+  },
+  {
+    "revision": "00738663e85bc7d72f4c",
+    "url": "/static/css/main.6db28778.chunk.css"
+  },
+  {
+    "revision": "f6c88135ba6daa2bed9710d18859b233",
+    "url": "/index.html"
+  }
+];
